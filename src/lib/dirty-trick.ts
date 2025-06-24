0312cc796400ca7a44d7d9b3d0ca155557905a7c
Zlatko Nikoloski <64633968+KZEN17@users.noreply.github.com>
2025-06-24 23:33:41 +0200

    first commit

--- src/lib/dirty-trick.ts
+++ src/lib/dirty-trick.ts
@@ -1,8 +1,8 @@
-import { atom } from "jotai";
+// import { atom } from "jotai";
 
-// Sometimes life is hard.
-// This is a dirty trick to clear the filter when the user clicks on the clear button.
-// For example on the player page.
-// It's mostly to avoid havign to pass each fields into the parent component. 
-// This is very dirty but the Scrum master will never know about it.
-export const dirtyClearFilterTrickTrigger = atom(false);
+// // Sometimes life is hard.
+// // This is a dirty trick to clear the filter when the user clicks on the clear button.
+// // For example on the player page.
+// // It's mostly to avoid havign to pass each fields into the parent component. 
+// // This is very dirty but the Scrum master will never know about it.
+// export const dirtyClearFilterTrickTrigger = atom(false);
--- src/utils/skins.ts
+++ src/utils/skins.ts
@@ -1,7 +1,7 @@
-import { InvTag } from "@/types/marketplace";
+// import { InvTag } from "@/types/marketplace";
 
 
-export function rarityBackground(tags: InvTag[]) {
-    const tag = tags.find((tag) => tag.category === "Rarity");
-    return `#${tag?.color}50`;
-  }
+// export function rarityBackground(tags: InvTag[]) {
+//     const tag = tags.find((tag) => tag.category === "Rarity");
+//     return `#${tag?.color}50`;
+//   }
