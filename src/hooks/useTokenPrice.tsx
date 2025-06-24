6e728c8e23a2d5b6ba423374c69120ab914fce1b
Zlatko Nikoloski <64633968+KZEN17@users.noreply.github.com>
2025-06-24 23:32:10 +0200

    first commit

--- src/hooks/useTokenPrice.tsx
+++ src/hooks/useTokenPrice.tsx
@@ -1,31 +1,31 @@
-import { useQuery } from "@tanstack/react-query";
-import { BASE_URL } from "@/lib/api/fetcher";
+// import { useQuery } from "@tanstack/react-query";
+// import { BASE_URL } from "@/lib/api/fetcher";
 
-const CACHE_TIME = 1000 * 60 * 5; // 5 minutes
+// const CACHE_TIME = 1000 * 60 * 5; // 5 minutes
 
-const fetchEthPrice = async (date: string | null) => {
-  const url = date ? `${BASE_URL}/crypto/eth-spot-price?date=${date}` : `${BASE_URL}/crypto/eth-spot-price`;
+// const fetchEthPrice = async (date: string | null) => {
+//   const url = date ? `${BASE_URL}/crypto/eth-spot-price?date=${date}` : `${BASE_URL}/crypto/eth-spot-price`;
 
-  const response = await fetch(url);
+//   const response = await fetch(url);
 
-  if (!response.ok) {
-    throw new Error(`Error status: ${response.statusText}`);
-  }
+//   if (!response.ok) {
+//     throw new Error(`Error status: ${response.statusText}`);
+//   }
 
-  return response.json();
-};
+//   return response.json();
+// };
 
-export const useTokenPrice = (date: string | null = null) => {
-  const {
-    data: ethPrice,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: ["ethPrice", date],
-    queryFn: () => fetchEthPrice(date),
-    staleTime: CACHE_TIME,
-    refetchOnWindowFocus: true,
-  });
+// export const useTokenPrice = (date: string | null = null) => {
+//   const {
+//     data: ethPrice,
+//     error,
+//     isLoading,
+//   } = useQuery({
+//     queryKey: ["ethPrice", date],
+//     queryFn: () => fetchEthPrice(date),
+//     staleTime: CACHE_TIME,
+//     refetchOnWindowFocus: true,
+//   });
 
-  return { ethPrice, error, isLoading };
-};
+//   return { ethPrice, error, isLoading };
+// };
