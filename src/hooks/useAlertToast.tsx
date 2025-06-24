7128fdbe248e96b65bb4175e0ba7454c95218e80
Zlatko Nikoloski <64633968+KZEN17@users.noreply.github.com>
2025-06-24 23:30:20 +0200

    first commit

--- src/hooks/useAlertToast.tsx
+++ src/hooks/useAlertToast.tsx
@@ -1,46 +1,46 @@
-import { useEffect } from "react";
-import Link from "next/link";
-import { toast } from "sonner";
+// import { useEffect } from "react";
+// import Link from "next/link";
+// import { toast } from "sonner";
 
-import { NotificationItem } from "../components/notifications/notification-list";
-import { useAlert } from "@/context/alerts-context";
-import { Alert, AlertType, BasicData } from "@/types/alert";
-import { NotificationData } from "@/types";
+// import { NotificationItem } from "../components/notifications/notification-list";
+// import { useAlert } from "@/context/alerts-context";
+// import { Alert, AlertType, BasicData } from "@/types/alert";
+// import { NotificationData } from "@/types";
 
-// TODO: Different styles for different alert types
-const useAlertToast = () => {
-  const { alert } = useAlert();
+// // TODO: Different styles for different alert types
+// const useAlertToast = () => {
+//   const { alert } = useAlert();
 
-  const alertContent = (alert: Alert) => {
-    if (alert.type === AlertType.NOTIFICATION) {
-      const alertData = alert.data as NotificationData;
-      return (
-        <div>
-          <NotificationItem notification={alertData} />
-        </div>
-      );
-    } else {
-      const alertData = alert.data as BasicData;
-      return (
-        <div>
-          <Link href={`${alertData.link}`}>
-            <p className="font-bold">{alertData.title}</p>
-            <p>{alertData.description}</p>
-          </Link>
-        </div>
-      );
-    }
-  };
+//   const alertContent = (alert: Alert) => {
+//     if (alert.type === AlertType.NOTIFICATION) {
+//       const alertData = alert.data as NotificationData;
+//       return (
+//         <div>
+//           <NotificationItem notification={alertData} />
+//         </div>
+//       );
+//     } else {
+//       const alertData = alert.data as BasicData;
+//       return (
+//         <div>
+//           <Link href={`${alertData.link}`}>
+//             <p className="font-bold">{alertData.title}</p>
+//             <p>{alertData.description}</p>
+//           </Link>
+//         </div>
+//       );
+//     }
+//   };
 
-  useEffect(() => {
-    if (alert) {
-      toast.info(alertContent(alert), {
-        unstyled: true,
-        position: "bottom-right",
-        classNames: { toast: "bg-greycontent/60 backdrop-blur-lg rounded-md p-2" },
-      });
-    }
-  }, [alert]);
-};
+//   useEffect(() => {
+//     if (alert) {
+//       toast.info(alertContent(alert), {
+//         unstyled: true,
+//         position: "bottom-right",
+//         classNames: { toast: "bg-greycontent/60 backdrop-blur-lg rounded-md p-2" },
+//       });
+//     }
+//   }, [alert]);
+// };
 
-export default useAlertToast;
+// export default useAlertToast;
